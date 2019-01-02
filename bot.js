4d7be8a3ae80c6899ac9760f143f4153cbc2d794
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2019-01-02 16:53:35 +0530

    commit changes

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ var qnaClient = new QnAClient({
 
 
 //for cosmos db
-//var azure = require('botbuilder-azure');
+var azure = require('botbuilder-azure');
 
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -75,16 +75,16 @@ var conversationid="conversationid";
 //for cosmos db
 
 
-//  var documentDbOptions = {
-//      host: 'https://gplflologiccosmosdbuat.documents.azure.com:443/', 
-//      masterKey: 'dmlyKuqhXlLQto7bY8tsZLJpM11Iq3x9FSKfllqZisN55YMrg18FfBJ6jh2u7JXWxAsnm44Um9iTijn4Geq77A==', 
-//      database: 'botdocs',   
-//      collection: 'botdata'
-//  };
+ var documentDbOptions = {
+     host: 'https://gplflologiccosmosdbuat.documents.azure.com:443/', 
+     masterKey: 'dmlyKuqhXlLQto7bY8tsZLJpM11Iq3x9FSKfllqZisN55YMrg18FfBJ6jh2u7JXWxAsnm44Um9iTijn4Geq77A==', 
+     database: 'botdocs',   
+     collection: 'botdata'
+ };
 
-// var docDbClient = new azure.DocumentDbClient(documentDbOptions);
+var docDbClient = new azure.DocumentDbClient(documentDbOptions);
 
-// var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
+var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
 
 //universal bot connection
@@ -92,8 +92,7 @@ const  bot = module.exports =  new builder.UniversalBot(connector, function (ses
      var reply = createEvent("changeBackground", session.message.text, session.message.address);
         session.endDialog(reply);
 
- });
-//.set('storage', cosmosStorage); 
+ }).set('storage', cosmosStorage); 
 
 
 //LUIS Connection
